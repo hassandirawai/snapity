cf97ae63f7f596d81418b5b547c87f4babbb43a5
vondoom78 <majorchaor88jp@gmail.com>
2025-12-27 03:03:36 +0300

    fix: Fix better-auth removing baseURL from auth-cleint.ts

--- app/utils/auth-client.ts
+++ app/utils/auth-client.ts
@@ -2,7 +2,6 @@ import { usernameClient } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/vue'
 
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.NUXT_PUBLIC_BETTER_AUTH_URL || '/',
   plugins: [
     usernameClient(),
   ],
