a5afe16f65a111d79712f2b07636088f4082d6a8
vondoom78 <majorchaor88jp@gmail.com>
2026-01-03 18:25:10 +0300

    Wrap post editor and for you and following feeds in ClientOnly component

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,25 +8,27 @@ useSeoMeta({
 <template>
   <main class="flex w-full min-w-0 gap-6">
     <div class="w-full flex flex-col space-y-6">
-      <PostEditor class="top-[5.54rem]" />
-      <div>
-        <Tabs default-value="for-you-feed">
-          <TabsList class="w-full">
-            <TabsTrigger value="for-you-feed">
-              For You
-            </TabsTrigger>
-            <TabsTrigger value="following-feed">
-              Following
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="for-you-feed">
-            <ForYouFeed />
-          </TabsContent>
-          <TabsContent value="following-feed">
-            <FollowingFeed />
-          </TabsContent>
-        </Tabs>
-      </div>
+      <ClientOnly>
+        <PostEditor class="top-[5.54rem]" />
+        <div>
+          <Tabs default-value="for-you-feed">
+            <TabsList class="w-full">
+              <TabsTrigger value="for-you-feed">
+                For You
+              </TabsTrigger>
+              <TabsTrigger value="following-feed">
+                Following
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="for-you-feed">
+              <ForYouFeed />
+            </TabsContent>
+            <TabsContent value="following-feed">
+              <FollowingFeed />
+            </TabsContent>
+          </Tabs>
+        </div>
+      </ClientOnly>
     </div>
     <TrendsSidebar />
   </main>
