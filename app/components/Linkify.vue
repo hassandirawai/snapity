0cf56abbebaa7332e63fd9a42e2b5659c4d92d7f
vondoom78 <majorchaor88jp@gmail.com>
2026-06-18 23:35:13 +0300

    Test if Cannot read properties of null error at Notifications page gone

--- app/components/Linkify.vue
+++ app/components/Linkify.vue
@@ -21,35 +21,37 @@ const parts = computed(() => {
 </script>
 
 <template>
-  <template
-    v-for="(part, index) in parts"
-    :key="`${part}-${index}`"
-  >
-    <a
-      v-if="part.startsWith('http')"
-      :href="part"
-      target="_blank"
-      rel="noopener noreferrer"
-      class="text-primary underline"
+  <ClientOnly>
+    <template
+      v-for="(part, index) in parts"
+      :key="`${part}-${index}`"
     >
-      {{ part }}
-    </a>
+      <a
+        v-if="part.startsWith('http')"
+        :href="part"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-primary underline"
+      >
+        {{ part }}
+      </a>
 
-    <UserMentionWithTooltip
-      v-else-if="part.startsWith('@')"
-      :username="part.slice(1)"
-    />
+      <UserMentionWithTooltip
+        v-else-if="part.startsWith('@')"
+        :username="part.slice(1)"
+      />
 
-    <NuxtLink
-      v-else-if="part.startsWith('#')"
-      :to="`/hashtag/${part.slice(1)}`"
-      class="text-primary hover:underline"
-    >
-      {{ part }}
-    </NuxtLink>
+      <NuxtLink
+        v-else-if="part.startsWith('#')"
+        :to="`/hashtag/${part.slice(1)}`"
+        class="text-primary hover:underline"
+      >
+        {{ part }}
+      </NuxtLink>
 
-    <span v-else>
-      {{ part }}
-    </span>
-  </template>
+      <span v-else>
+        {{ part }}
+      </span>
+    </template>
+  </ClientOnly>
 </template>
