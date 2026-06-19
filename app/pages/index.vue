92f5e2ea9581f71b595779406d3d773d71467609
vondoom78 <majorchaor88jp@gmail.com>
2026-06-19 21:07:22 +0300

    Fix reading ce error #12

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,6 +6,8 @@ import {
   TabsTrigger,
 } from 'reka-ui'
 
+const tab = ref('a')
+
 useSeoMeta({
   title: 'Home',
   description: 'Snapity is a social media platform that allows users to connect with each other and share their thoughts, ideas, and experiences.',
@@ -15,25 +17,46 @@ useSeoMeta({
 <template>
   <main class="flex w-full min-w-0 gap-6">
     <div class="w-full flex flex-col space-y-6">
+      <ClientOnly>
+        <PostsEditor />
+        <template #fallback>
+          <div class="flex flex-col gap-6 bg-card border rounded-2xl p-6">
+            <div class="flex gap-6">
+              <Skeleton class="hidden sm:block size-12 shrink-0 rounded-full" />
+              <div class="relative w-full max-w-full overflow-x-auto">
+                <Skeleton class="w-full h-12 rounded-2xl" />
+              </div>
+            </div>
+            <div class="flex justify-end items-center gap-3">
+              <Skeleton class="w-6 h-6 rounded-full" />
+              <Skeleton class="w-18 h-10 px-4 py-2" />
+            </div>
+          </div>
+        </template>
+      </ClientOnly>
       <div>
-        <TabsRoot default-value="for-you-feed">
-          <TabsList class="w-full">
-            <TabsTrigger value="for-you-feed">
-              For You
+        <TabsRoot v-model="tab">
+          <TabsList>
+            <TabsTrigger value="a">
+              A
             </TabsTrigger>
-            <TabsTrigger value="following-feed">
-              Following
+
+            <TabsTrigger value="b">
+              B
             </TabsTrigger>
           </TabsList>
-          <TabsContent value="for-you-feed">
+
+          <TabsContent value="a">
             <ForYouFeed />
           </TabsContent>
-          <TabsContent value="following-feed">
+
+          <TabsContent value="b">
             <FollowingFeed />
           </TabsContent>
         </TabsRoot>
       </div>
     </div>
+    <TrendsSidebar />
   </main>
 </template>
 
