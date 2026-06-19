c3b4cbbefdfdfa45ebc02fd9da3a8f90849b7b90
vondoom78 <majorchaor88jp@gmail.com>
2026-06-19 20:54:31 +0300

    Fix reading ce error #12

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -15,23 +15,6 @@ useSeoMeta({
 <template>
   <main class="flex w-full min-w-0 gap-6">
     <div class="w-full flex flex-col space-y-6">
-      <ClientOnly>
-        <PostsEditor />
-        <template #fallback>
-          <div class="flex flex-col gap-6 bg-card border rounded-2xl p-6">
-            <div class="flex gap-6">
-              <Skeleton class="hidden sm:block size-12 shrink-0 rounded-full" />
-              <div class="relative w-full max-w-full overflow-x-auto">
-                <Skeleton class="w-full h-12 rounded-2xl" />
-              </div>
-            </div>
-            <div class="flex justify-end items-center gap-3">
-              <Skeleton class="w-6 h-6 rounded-full" />
-              <Skeleton class="w-18 h-10 px-4 py-2" />
-            </div>
-          </div>
-        </template>
-      </ClientOnly>
       <div>
         <TabsRoot default-value="for-you-feed">
           <TabsList class="w-full">
@@ -51,7 +34,6 @@ useSeoMeta({
         </TabsRoot>
       </div>
     </div>
-    <TrendsSidebar />
   </main>
 </template>
 
