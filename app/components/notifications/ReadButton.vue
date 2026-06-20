b6e455d5cb11f11a52ed0e14a2663fc777518d0a
vondoom78 <majorchaor88jp@gmail.com>
2026-06-20 19:10:06 +0300

    Rendering NotificationsReadButton during SSR

--- app/components/notifications/ReadButton.vue
+++ app/components/notifications/ReadButton.vue
@@ -18,45 +18,26 @@ const { data: notificationsCountInfo } = useQuery({
 </script>
 
 <template>
-  <ClientOnly>
-    <template #fallback>
-      <Button
-        variant="ghost"
-        title="Messages"
-        class="flex items-center justify-start gap-3"
-        as-child
-      >
-        <NuxtLink to="/messages">
-          <Icon
-            name="fluent:alert-20-regular"
-            class="text-xl"
-          />
-          <span class="hidden lg:inline">Notifications</span>
-        </NuxtLink>
-      </Button>
-    </template>
-
-    <Button
-      variant="ghost"
-      title="Notifications"
-      class="flex items-center justify-start gap-3"
-      as-child
-    >
-      <NuxtLink to="/notifications">
-        <div class="relative">
-          <Icon
-            name="fluent:alert-20-regular"
-            class="text-xl"
-          />
-          <span
-            v-if="notificationsCountInfo?.unreadCount > 0"
-            class="absolute -right-1 -top-1 rounded-full bg-primary text-primary-foreground px-1 text-xs font-medium tabular-nums"
-          >
-            {{ notificationsCountInfo.unreadCount }}
-          </span>
-        </div>
-        <span class="hidden lg:inline">Notifications</span>
-      </NuxtLink>
-    </Button>
-  </ClientOnly>
+  <Button
+    variant="ghost"
+    title="Notifications"
+    class="flex items-center justify-start gap-3"
+    as-child
+  >
+    <NuxtLink to="/notifications">
+      <div class="relative">
+        <Icon
+          name="fluent:alert-20-regular"
+          class="text-xl"
+        />
+        <span
+          v-if="notificationsCountInfo?.unreadCount > 0"
+          class="absolute -right-1 -top-1 rounded-full bg-primary text-primary-foreground px-1 text-xs font-medium tabular-nums"
+        >
+          {{ notificationsCountInfo.unreadCount }}
+        </span>
+      </div>
+      <span class="hidden lg:inline">Notifications</span>
+    </NuxtLink>
+  </Button>
 </template>
